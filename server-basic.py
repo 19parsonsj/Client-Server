4f45f17bdb107ff3d6342de3cd6d86a0c1f42d1d
19parsonsj <79724637+19parsonsj@users.noreply.github.com>
2022-04-21 22:11:23 -0400

    Update server-basic.py

--- server-basic.py
+++ server-basic.py
@@ -1,90 +1,131 @@
 import os
 import socket
-from time import sleep
-from time import time
+import threading
+import ftplib
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+# IP =  "192.168.1.101" #"localhost"
 IP = "localhost"
 PORT = 4451
 ADDR = (IP,PORT)
-SIZE = 1024 ## byte .. buffer size
+SIZE = 1024
 FORMAT = "utf-8"
-SERVER_DATA_PATH = "server_data"
-def main():
-    
-    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    client.connect(ADDR)
-    while True:  ### multiple communications
-        data = client.recv(SIZE).decode(FORMAT)
-        cmd, msg = data.split("@")
-        
-        if cmd == "OK":
-            print(f"{msg}")
-        elif cmd == "DISCONNECTED":
-            print(f"{msg}")
-            break
-        
-        data = input("> ") 
-        data = data.split(" ")
-        cmd = data[0]
-        
-        if cmd == "TASK":
-            client.send(cmd.encode(FORMAT))
-        elif cmd == "LOGOUT":
-            client.send(cmd.encode(FORMAT))
-            break
+SERVER_PATH = "server"
+### to handle the clients
+def handle_client (conn,addr):
 
+    print(f"[NEW CONNECTION] {addr} connected.")
+    conn.send("OK@Welcome to the server".encode(FORMAT))
+    #double array for each file date time 
+    
+    while True:
+        data =  conn.recv(SIZE).decode(FORMAT)
+        data = data.split("@")
+        cmd = data[0]
+       
+        send_data = "OK@"
+        if cmd == "LOGOUT":
+            break
+        elif cmd == "TASK": 
+            send_data += "LOGOUT from the server.\n"
+            conn.send(send_data.encode(FORMAT))
+            
+            
+            
         elif cmd == "CREATE":
-            client.send(cmd.encode(FORMAT))
+            send_data += "Creating file. \n"
+            conn.send(send_data.encode(FORMAT))
+            path = '/Users/jasmineparsons'
+            fileName = 'Simple-text-file.txt'
+            buff = "ABCD \n"
+            with open(os.path.join(path,fileName), 'w') as temp_file: ##### creating the file
+                temp_file.write(buff)
+            print("The file does not exist")
             
             
-        #> UPLOAD /Users/jasmineparsons/filename
+            
+            print(f"{addr} disconnected")
+            conn.close()
+        
         elif cmd == "UPLOAD":
+            #add gui to choose 
+            name, text = data[1], data[2]
             
+            filepath = os.path.join(SERVER_PATH, name)
+        
+            with open(filepath, "w") as f:
+                f.write(text)
+
+            send_data = "OK@File uploaded successfully."
+            
+            conn.send(send_data.encode(FORMAT))
+            os.close(f.fileno())
+            
+            
+            
+        elif cmd == "LIST":
+            files = os.listdir(SERVER_PATH)
+            send_data = "OK@"
+
+            if len(files) == 0:
+                send_data += "The server directory is empty"
+            else:
+                send_data += "\n".join(f for f in files)
+                
+            conn.send(send_data.encode(FORMAT))
+            
+            
+            
+            
+        elif cmd == "DELETE":
+            files = os.listdir(SERVER_PATH)
+            send_data = "OK@"
+            filename = data[1]
+
+            if len(files) == 0:
+                send_data += "The server directory is empty"
+            else:
+                if filename in files:
+                    os.system(f"rm {SERVER_PATH}/{filename}")
+                    send_data += "File deleted successfully."
+                else:
+                    send_data += "File not found."
+
+            conn.send(send_data.encode(FORMAT))
+            
+            
+            
+            
+            
+        elif cmd == "DOWNLOAD":
             path1 = data[1]
            
             with open(f"{path1}", "r") as f:
                 text = f.read()
             filename = path1.split("/")[-1]
-            send_data = f"{cmd}@{filename}@{text}"
-            client.send(send_data.encode(FORMAT))
+            send_data = f"{filename}@{text}"
+            conn.send(send_data.encode(FORMAT))
+            
+                
+            
+            
+            
+            
+     
+    print(f"{addr} disconnected")
+    conn.close()
         
-        elif cmd == "LIST":
-            #redo format to add size, upload date and time, number of downloads
-            client.send(cmd.encode(FORMAT))
-            
-            
-        elif cmd == "DELETE":
-            client.send(f"{cmd}@{data[1]}".encode(FORMAT))
-            
-            
-        elif cmd == "DOWNLOAD":
-            filename = data[1]
-            send_data = f"{cmd}@{filename}"
-            client.send(send_data.encode(FORMAT))
-            data =  client.recv(SIZE).decode(FORMAT)
-            data = data.split("@")
-            filename = data[0]
-            text = data[1]
-            
-            filepath = os.path.join(SERVER_DATA_PATH, filename)
-        
-            with open(filepath, "w") as f:
-                f.write(text)
-            f.close()
-            data = input("> ") 
-            data = data.split(" ")
-            cmd = data[0]
-            
-            
-
-            
-            
-            
-                    
-            
-        
-
-    print("Disconnected from the server.")
-    client.close() ## close the connection
+def main():
+    print("Starting the server")
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM) ## used IPV4 and TCP connection
+    server.bind(ADDR) # bind the address
+    server.listen() ## start listening
+    print(f"server is listening on {IP}: {PORT}")
+    while True:
+        conn, addr = server.accept() ### accept a connection from a client
+        thread = threading.Thread(target = handle_client, args = (conn, addr)) ## assigning a thread for each client
+        thread.start()
 
 if __name__ == "__main__":
     main()
